41688ca86b43d22ca267d0adcee0d9fe91576a67
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2023-10-09 11:10:29 +0200

    Add files via upload

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+const refs = {
+  btnStart: document.querySelector("[data-start]"),
+  btnStop: document.querySelector("[data-stop]"),
+  body: document.querySelector("body"),
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let timer = 0;
+
+const setColorChange = function () {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  timer = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.btnStart.setAttribute("disabled", "disabled");
+  refs.btnStop.removeAttribute("disabled");
+};
+const stopColorChange = function () {
+  clearInterval(timer);
+
+  refs.btnStop.setAttribute("disabled", "disabled");
+  refs.btnStart.removeAttribute("disabled");
+};
+
+refs.btnStart.addEventListener("click", setColorChange);
+refs.btnStop.addEventListener("click", stopColorChange);
